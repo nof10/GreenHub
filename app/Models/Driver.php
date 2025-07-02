b9d825df631eba511a7bdfa5befc73ceb67943bb
Nofa Osamah <nofa.os111@gmail.com>
2025-07-02 14:29:07 +0300

    Some changes

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
 
 
 class Driver extends Authenticatable
