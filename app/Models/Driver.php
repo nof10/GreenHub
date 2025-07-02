ef0b6cd97bb0f9e0dd75abc4f7c2891f2418bc66
Nofa Osamah <nofa.os111@gmail.com>
2025-07-02 13:34:13 +0300

    Some Changes

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Authenticatable;
 
-class Driver extends Model
+
+class Driver extends Authenticatable
 {
     //
 }
