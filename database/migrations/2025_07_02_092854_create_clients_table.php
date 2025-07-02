3d5241452ece982c9e0c4ac8fe1f9ab78f05c1ef
Nofa Osamah <nofa.os111@gmail.com>
2025-07-02 14:09:04 +0300

    Some changes

--- database/migrations/2025_07_02_092854_create_clients_table.php
+++ database/migrations/2025_07_02_092854_create_clients_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->timestamps();
             $table->string('phone')->unique();
             $table->string('status')->nullable();
